d9bc82c198f7785e2da5a5fe2f82295c25c3c033
jesseojala20-crypto <jesseojala20@gmail.com>
2025-10-12 19:02:26 +0300

    Updated TriggerComponent
    Made an error in the last addition. We have to check also if the pointer value is true, otherwise our code could not work.

--- Source/DungeonEscape/TriggerComponent.cpp
+++ Source/DungeonEscape/TriggerComponent.cpp
@@ -45,11 +45,14 @@ void UTriggerComponent::TickComponent(float DeltaTime, ELevelTick TickType, FAct
 
 void UTriggerComponent::OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
-	if (OtherActor->ActorHasTag("PressurePlateActivator")) //This forces the game to check if the colliding actor is the player
+	if (OtherActor)
 	{
-		if (Mover)
+		if (OtherActor->ActorHasTag("PressurePlateActivator")) //This forces the game to check if the colliding actor is the player
 		{
-			Mover->bShouldMove = true; //on collision this  function will be called.
+			if (Mover)
+			{
+				Mover->bShouldMove = true; //on collision this  function will be called.
+			}
 		}
 	}
 }
@@ -57,11 +60,12 @@ void UTriggerComponent::OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AAct
 
 void UTriggerComponent::OnOverlapEnd(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
 {
-	if (OtherActor->ActorHasTag("PressurePlateActivator")) //This forces the game to check if the colliding actor is the player
-	{
-		if (Mover)
+	if (OtherActor)
+		if (OtherActor->ActorHasTag("PressurePlateActivator")) //This forces the game to check if the colliding actor is the player
 		{
-			Mover->bShouldMove = false; //on collision this  function will be called.
+			if (Mover)
+			{
+				Mover->bShouldMove = false; //on collision this  function will be called.
+			}
 		}
-	}
 }
